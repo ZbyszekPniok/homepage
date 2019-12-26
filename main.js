27c23f32978cec6f728f34156d156ab0f74eadfd
Zbyszek Pniok <z.pniok@gmail.com>
2019-12-26 17:06:02 -0500

    initial commmit

---
+++ main.js
@@ -0,0 +1,6 @@
+console.log ('Hello!');
+const name='Zbyszek Pniok';
+const age=39;
+
+console.log (`My name is ${name} and I am ${age} years old`);
+console.log ('My name is ' +name+ ` and I am ` +age+ ` years old`);
