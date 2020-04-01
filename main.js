6c70363286f26387c515a968929052b81ccd6420
Zbyszek Pniok <z.pniok@gmail.com>
2020-04-01 15:38:58 -0400

    Refactoring for BEM

--- main.js
+++ main.js
@@ -4,3 +4,18 @@ const age=39;
 
 console.log (`My name is ${name} and I am ${age} years old`);
 console.log ('My name is ' +name+ ` and I am ` +age+ ` years old`);
+
+const header = document.querySelector('.header--js');
+console.log (header);
+
+const about = document.querySelector('.about__header');
+console.log (about.innerHTML);
+about.innerHTML = `My name is ${name} and I am ${age} years old`
+
+const articles = document.querySelectorAll('.article__paragraph');
+console.log (articles);
+
+const paragraphs = document.querySelectorAll('p');
+console.log (paragraphs[2]);
+
+
